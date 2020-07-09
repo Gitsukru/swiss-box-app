8c00a383ae679ffa3198789303b057cf479964be
Gitsukru <suisse1022@gmail.com>
2020-07-09 18:58:19 +0200

    Projeye react kuruldu ve Router integre edildi

---
+++ src/Components/Pages/PriceList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function PriceList() {
+    return (
+        <div>
+            Home     Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Aperiam, voluptatum autem ea magni fugit sint ullam esse temporibus perspiciatis eveniet. 
+            Soluta, suscipit eius deserunt corrupti numquam quo tempore doloremque vel.
+        </div>
+    )
+}
